7a6fdbc8044133bc8b7248a6ba99e802a67a2bf5
Jaysonliu <leobest9688@gmail.com>
2021-08-14 23:49:34 +0800

    working with lists

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 function Header(props) {
-  console.log(props);
+  // console.log(props);
   return (
     <header>
       <h1>{props.name}'s Kitchen</h1>
@@ -9,27 +9,42 @@ function Header(props) {
   );
 }
 
-function Main() {
+function Main(props) {
   return (
     <section>
-      <p>We serve the most delicious food around.</p>
+      <p>We serve the most {props.adjective} food around.</p>
+      <ul style={{ textAlign: "left" }}>
+        {props.dishes.map((dish) => 
+        <li>{dish}</li>
+        )}
+      </ul>
     </section>
+
   );
 }
 
-function Footer() {
+function Footer(props) {
   return (
     <footer>
-      <p>It's true.</p>
+      <p>Copyright {props.year}</p>
     </footer>
   );
 }
+
+const dishes = [
+  "Black bean beaf",
+  "Peiking duck",
+  "Mixed vegitables"
+];
+
+// dishes.map((dish) => console.log(dish) );
+
 function App() {
   return (
     <div className="App">
       <Header name="Jayson" />
-      <Main />
-      <Footer />
+      <Main adjective="amazing" dishes={dishes}/>
+      <Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
